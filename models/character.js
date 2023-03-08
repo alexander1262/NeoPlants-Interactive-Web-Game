011230a227a85e9792e90c51e33e7c488f6c2ddc
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-03-07 19:23:12 -0500

    added models and associations in models folder

---
+++ models/character.js
@@ -0,0 +1,46 @@
+/* eslint-disable indent */
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Character extends Model { }
+
+Character.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        charName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2, 15],
+            },
+        },
+        charType: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'license',
+    }
+);
+
+module.exports = Character;
