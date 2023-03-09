64af7c6060735a0c31979acf0fc60728e0ab51e9
Sander Weiss <weisssander@gmail.com>
2023-03-08 21:03:24 -0500

    login displays

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
 
 router.post('/signup', async (req, res) => {
-    const newUserData = req.body;
-    res.send(201).json({ message: "New user created!" })
-})
-
-router.get("/", async (req, res) => {
-    res.render("home")
+  const newUserData = req.body;
+  res.send(201).json({ message: 'New user created!' });
 });
 
-router.get("/signup", async (req, res) => {
-    res.render('signup');
-})
+router.get('/', async (req, res) => {
+  res.render('home');
+});
+
+router.get('/signup', async (req, res) => {
+  res.render('signup');
+});
+
+router.get('/login', async (req, res) => {
+  res.render('login');
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const htmlRoutes = require('./htmlRoutes')
+const htmlRoutes = require('./htmlRoutes');
 // const apiRoutes = require('./api');
 
 router.use('/', htmlRoutes);
