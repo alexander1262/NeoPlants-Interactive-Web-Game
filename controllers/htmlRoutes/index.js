2d2eac264fc167bebee74c4f93185db46823c744
Sander Weiss <weisssander@gmail.com>
2023-03-08 19:04:45 -0500

    changed login to signup handlebars

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 
-router.get("/", async (req, res) => {
-    res.render("home")
-});
-
-router.get("/login", async (req, res) => {
-    res.render('login');
+router.post('login', async (req, res) => {
+    const newUserData = req.body;
+    res.send(201).json({ message: "New user created!" })
 })
+
+// router.get("/", async (req, res) => {
+//     res.render("home")
+// });
+
+// router.get("/login", async (req, res) => {
+//     res.render('login');
+// })
 
 module.exports = router;
---
+++ public/js/signup.js
@@ -0,0 +1,35 @@
+const signupButton = document.querySelector(".form-signup")
+
+async function handleSignup(event) {
+    event.preventDefault();
+    
+    const userNameValue = document.querySelector('#username-signup').value.trim();
+    const passwordValue = document.querySelector('#password-signup').value.trim();
+    const emailValue = document.querySelector('#email-signup').value.trim();
+
+    const newUser = {
+        username: userNameValue,
+        email: emailValue,
+        password: passwordValue,
+    };
+
+    await fetch("/api/signup", {
+        body: JSON.stringify(newUser),
+        method: "POST",
+        headers: {
+            "content-type": 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        console.log('Signed Up');
+      } else {
+        console.log('Not Signed Up');
+      }
+
+    console.log({ userNameValue, passwordValue, emailValue});
+
+    signupForm.reset();
+}
+
+signupButton.addEventListener("submit", handleSignup)
