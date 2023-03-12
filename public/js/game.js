88a3dfedba4239948181dd25324e213f13cac168
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-03-12 13:57:14 -0400

    game js

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,42 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-inner-declarations */
+/* eslint-disable prettier/prettier */
+const life = 50;
+const progressBar = document.createElement('div');
+progressBar.id = 'myBar';
+progressBar.innerHTML = '10%';
+progressBar.style.color = 'red';
+document.body.prepend(progressBar);
+
+
+// function decay() {
+//     if (life === 100) {
+//         const elem = document.getElementById('myBar');
+//         let width = 99;
+//         const id = setInterval(frame, 1000);
+//         function frame() {
+//             if (width >= 100 || width <= 0) {
+//                 clearInterval(id);
+//                 alert('You killed me!')
+//             } else {
+//                 width--;
+//                 elem.style.width = `${width}%`;
+//                 elem.innerHTML = `${width}%`;
+//             }
+//         }
+//     }
+// }
+
+function health() {
+    let i = 20;
+    ++i;
+    const elem = document.getElementById('myBar');
+    elem.style.width = i;
+    elem.innerHTML = i;
+    console.log(i);
+
+};
+
 const feedMeButton = document.createElement('button');
 feedMeButton.id = 'feedMe';
 feedMeButton.innerHTML = 'Feed Me Seymour!';
@@ -19,3 +58,14 @@ sleepButton.id = 'feedMe';
 sleepButton.innerHTML = "I'm tired!";
 sleepButton.style.background = 'white';
 document.body.appendChild(sleepButton);
+
+
+
+
+// decay();
+
+
+
+// feedMeButton.addEventListener('click', health());
+// playButton.addEventListener('click', health(life));
+// musicButton.addEventListener('click', health(life));
