d5a90dc0f5af496e05afb9f8503a0ca88cc94de4
Franksu <fsuriel@gmail.com>
2023-03-15 14:27:09 -0400

    Added day and night, also sun and moon shows behind the clouds

--- public/js/sunMoon.js
+++ public/js/sunMoon.js
@@ -4,22 +4,22 @@ const sun = function () {
   anime({
     targets: '.sun',
     translateX: 4560,
-    translateY: 1000,
+    translateY: 800,
     easing: 'easeInOutSine',
-    duration: 41600,
+    duration: 41600,    
   });
 };
 const moon = function () {
   anime({
     targets: '.moon',
     translateX: 4560,
-    translateY: 1000,
+    translateY: 800,
     easing: 'easeInOutSine',
-    duration: 41600,
+    duration: 41600,    
   });
 };
 
-let seconds = 116;
+let seconds = 90;
 
 // setInterval(sun, seconds)
 
@@ -28,26 +28,30 @@ const movement = function () {
   const timer = setInterval(() => {
     seconds -= 1;
     console.log(seconds);
-    if (seconds === 114) {      
+    if (seconds === 88) {
+      sunDisplay.style.visibility = 'hidden';
       sun();
     }
-    if (seconds === 58) {
+    if (seconds === 48) {      
+      document.body.style.backgroundImage = 'url(../images/newnightskytest.png)';
       moon();
     }
-    if (seconds === 10) {         
+    if (seconds === 10) {
       anime({
-        targets: '.sun',        
-        translateX: -560,
+        targets: '.sun',
+        translateX: -150,        
       });
     }
     
-    if (seconds === 0) {
+    if (seconds === 0) {      
+      document.body.style.backgroundImage = 'url(../images/newdayskytest.png)';
       anime({
-        targets: '.moon',        
-        translateX: -560,
+        targets: '.moon',
+        translateX: -150,
       });
+
       clearInterval(timer);
-      seconds = 116;
+      seconds = 90;
       movement();
     }
   }, 1000);
