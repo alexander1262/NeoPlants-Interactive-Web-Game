287267717770e52ba7d7ba63f37514b3a19c226b
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-03-16 19:09:32 -0400

    added code to create character on user sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -7,7 +7,10 @@ async function handleSignup(event) {
   const userNameValue = document.querySelector('#username-signup').value.trim();
   const passwordValue = document.querySelector('#password-signup').value.trim();
   const emailValue = document.querySelector('#email-signup').value.trim();
-
+  const characterName = document.querySelector('#char-name').value.trim();
+  const characterType = document.querySelector('#cactus').value;
+  console.log(characterName);
+  console.log(characterType);
   const newUser = {
     username: userNameValue,
     email: emailValue,
@@ -25,6 +28,27 @@ async function handleSignup(event) {
   if (response.ok) {
     console.log('Signed Up');
     document.getElementById('signedUpP').innerHTML = 'Successfully signed up!';
+  } else {
+    console.log('Not Signed Up');
+  }
+
+  const newCharacter = {
+    username: userNameValue,
+    email: emailValue,
+    charName: characterName,
+    charType: characterType,
+  };
+  const createChar = await fetch('/api/character/signup', {
+    body: JSON.stringify(newCharacter),
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  console.log(newCharacter);
+  if (createChar.ok) {
+    console.log('Signed Up');
+    document.getElementById('signedUpP').innerHTML = 'Successfully created character!';
   } else {
     console.log('Not Signed Up');
   }
