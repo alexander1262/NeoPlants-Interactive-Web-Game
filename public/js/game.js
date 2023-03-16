6cf89474ca45b56a8c0c32f766e1559dd171c409
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-03-16 17:54:56 -0400

    eliminated useless code

--- public/js/game.js
+++ public/js/game.js
@@ -3,7 +3,6 @@
 /* eslint-disable prettier/prettier */
 const progressBar = document.createElement('div');
 progressBar.id = 'myBar';
-// progressBar.innerHTML = 0;
 progressBar.style.color = 'red';
 document.body.prepend(progressBar);
 
@@ -77,32 +76,6 @@ const feedMeButton = document.getElementById('feedMe');
 const playButton = document.getElementById('play');
 const musicButton = document.getElementById('dance');
 const sleepButton = document.getElementById('sleep');
-// const playButtonsDiv = document.createElement('div.buttonWrapper');
-
-// const feedMeButton = document.createElement('button');
-// feedMeButton.id = 'feedMe';
-// feedMeButton.innerHTML = 'Feed Me Seymour!';
-// // document.body.appendChild(feedMeButton);
-
-// const playButton = document.createElement('button');
-// playButton.id = 'play';
-// playButton.innerHTML = 'Can We Play!';
-// // document.body.appendChild(playButton);
-
-// const musicButton = document.createElement('button');
-// musicButton.id = 'dance';
-// musicButton.innerHTML = 'Dont stop rockin!';
-// // document.body.appendChild(musicButton);
-
-// const sleepButton = document.createElement('button');
-// sleepButton.id = 'sleep';
-// sleepButton.innerHTML = "I'm tired!";
-// sleepButton.style.background = 'white';
-// document.body.appendChild(sleepButton);
-
-
-
-
 
 feedMeButton.addEventListener('click', () => health());
 playButton.addEventListener('click', () => health());
