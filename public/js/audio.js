5a6b918ef86947e599b40403e50aae5eb852554a
Luna Rae <kra7888@gmail.com>
2023-03-18 12:45:10 -0400

    updated audio

--- public/js/audio.js
+++ public/js/audio.js
@@ -6,7 +6,7 @@ function toggleMusic() {
   if (music.paused) {
     music.play();
     music.loop = true;
-    musicToggle.innerTest = 'Stop Music';
+    musicToggle.innerText = 'Pause Music';
   } else {
     music.pause();
     musicToggle.innerText = 'Start Music';
@@ -18,3 +18,24 @@ musicToggle.addEventListener('click', toggleMusic);
 volumeRange.addEventListener('input', () => {
   music.volume = volumeRange.value;
 });
+
+const audio = document.getElementById('music');
+const sources = document.querySelectorAll('.play-list');
+const skip = document.getElementById('skip');
+const back = document.getElementById('back');
+let currentSource = 0;
+
+function skipTrack() {
+  currentSource = (currentSource + 1) % sources.length;
+  audio.src = sources[currentSource].src;
+  audio.play();
+}
+
+function backTrack() {
+  currentSource = (currentSource - 1) % sources.length;
+  audio.src = sources[currentSource].src;
+  audio.play();
+}
+
+skip.addEventListener('click', skipTrack);
+back.addEventListener('click', backTrack);
